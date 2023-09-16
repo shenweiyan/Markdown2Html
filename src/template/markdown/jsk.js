5791b33b977fed832fbde4abccf48aba19306c57
shenweiyan <weiyanshum@gmail.com>
2023-09-16 15:38:19 +0800

    技术控主题增加增加背景网格

--- src/template/markdown/jsk.js
+++ src/template/markdown/jsk.js
@@ -6,6 +6,10 @@ export default `/*自定义样式，实时生效*/
  * 英文换行 word-break: break-all;
  */
 #nice {
+  /*增加背景网格*/
+  background-image: linear-gradient(90deg, rgba(50, 0, 0, 0.05) 3%, rgba(0, 0, 0, 0) 3%), linear-gradient(360deg,rgba(50, 0, 0, 0.05) 3%, rgba(0, 0, 0, 0) 3%);
+  background-size: 20px 20px;
+  background-position: center center;
 }
 
 /* 段落，下方未标注标签参数均同此处
