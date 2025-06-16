b6c20f1cfb021a4be0b33a06eb99aab695a19c59
kebinzhi <251112375@qq.com>
2025-06-16 09:53:34 +0800

    Add files via upload

---
+++ src/template/basic_media.js
@@ -0,0 +1,12 @@
+export default `
+@media (min-width: 768px) {
+	#nice { line-height: 1.68; }
+	#nice p { line-height: 26px; }
+	#nice li section { line-height: 26px; }
+	#nice blockquote p { line-height: 26px; }
+	#nice pre code span { line-height: 26px; }
+	#nice .code-snippet__fix .code-snippet__line-index li { line-height: 26px; }
+	#nice .footnote-num { line-height: 26px; }
+	#nice .footnote-item p { line-height: 26px; }
+}
+`;
